3d8adc2352db1957d98c32066edf53d4995fa0a7
Craig Mulligan <hobochildster@gmail.com>
2017-12-10 16:29:19 +0000

    init

---
+++ scripts/install.sh
@@ -0,0 +1,8 @@
+# Deps
+apt-get update && apt-get install git make curl zsh vim-gnome python nodejs npm docker jq
+# oh-my-zsh 
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+npm i -g yarn n
+# Install nightly node v
+n latest
