c8f0169921cf970f11b349a905d048c74c5bc49c
Craig Mulligan <hobochildster@gmail.com>
2017-12-11 10:50:57 +0000

    add docker

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 # Deps
-apt-get update && apt-get install git make curl zsh vim-gnome python nodejs npm docker jq ack-grep mutt tmux w3m todotxt-cli ispell urlview offlineimap msmtp
+apt-get update && apt-get install git make curl zsh vim-gnome python nodejs npm docker jq ack-grep mutt tmux w3m todotxt-cli ispell urlview offlineimap msmtp linux-image-extra-$(uname -r) linux-image-extra-virtual
+
+# Install docker
+sh -c "$(curl -sSL https://get.docker.com/)"
+
 # oh-my-zsh 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
