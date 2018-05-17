7c91c48e58a6d3b33c869ea76672a81588093a36
Craig Mulligan <hobochildster@gmail.com>
2018-05-17 14:48:38 +0100

    add auto completes

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ sh -c "$(curl -sSL https://get.docker.com/)"
 # oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
 git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
 
 npm i -g yarn n
