46bea64f1c9f78d04f298c3b0c78d158b72cf01f
Craig Mulligan <hobochildster@gmail.com>
2019-05-28 16:51:42 +0100

    improve vim search

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 # Deps
-apt-get update && apt-get install git make curl zsh vim-gnome python nodejs npm docker jq mutt tmux w3m todotxt-cli ispell urlview offlineimap msmtp linux-image-extra-$(uname -r) linux-image-extra-virtual tmux
+apt-get update && apt-get install git make curl zsh vim-gnome python nodejs npm docker jq mutt tmux w3m todotxt-cli ispell urlview offlineimap msmtp linux-image-extra-$(uname -r) linux-image-extra-virtual tmux silversearcher-ag
 
 git clone https://github.com/jimeh/tmux-themepack.git ~/.tmux-themepack
 
