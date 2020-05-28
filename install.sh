61c5c2eff7c208c0678d2bf17aab4ecbd1e1837b
Craig Mulligan <hobochildster@gmail.com>
2020-05-28 17:55:13 +0200

    minimalist

--- install.sh
+++ install.sh
@@ -1,56 +1,37 @@
-# https://github.com/docker/for-linux/issues/833
-# Docker + ubuntu 19 issue
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   disco \
-   stable"
+# needed for add-apt-repository
+sudo apt-get update -y && \
+  sudo apt-get install -y software-properties-common
+
+# Add third-party repos
+sudo add-apt-repository ppa:jonathonf/vim
 
 # Deps
-sudo apt-get update && \
-  apt-get install git \
+sudo apt-get update -y && \
+  sudo apt-get install -y git \
   make \
   curl \
   zsh \
   vim \
+  vim-gtk \
   python \
-  nodejs \
-  npm \
   jq \
   mutt \
-  tmux \
-  w3m \
-  todotxt-cli \
-  ispell \
-  urlview \
   offlineimap \
   msmtp \
   tmux \
   xclip \
-  vim-gtk \
-  docker-ce \
-  docker-ce-cli \
-  containerd.io \
   silversearcher-ag \
-  lastpass-cli
-
-git clone https://github.com/jimeh/tmux-themepack.git ~/.tmux-themepack
+  lastpass-cli \
+  docker.io
 
 # oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-
-wget https://github.com/containerd/containerd/releases/download/v1.3.0/containerd-1.3.0.linux-amd64.tar.gz
-tar xvf containerd-1.3.0.linux-amd64.tar.gz
-wget "https://download.docker.com/linux/ubuntu/dists/disco/pool/stable/amd64/docker-ce-cli_19.03.3~3-0~ubuntu-disco_amd64.deb"
-wget "https://download.docker.com/linux/ubuntu/dists/disco/pool/stable/amd64/docker-ce_19.03.3~3-0~ubuntu-disco_amd64.deb"
-sudo apt install "containerd-1.3.0.linux-amd64.deb"
-sudo apt install "docker-ce-cli_19.03.3~3-0~ubuntu-disco_amd64.deb"
-sudo apt install "docker-ce_19.03.3~3-0~ubuntu-disco_amd64.deb"
-
 git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
 git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
 
+# Latest Node
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+sudo apt-get install -y nodejs
 
 sudo groupadd docker && sudo usermod -aG docker $USER
