8490ca1d6687c5d457ad4f2c523d3a719e9394b4
Craig Mulligan <git@craigmulligan.com>
2023-11-04 12:01:33 -0700

    rm install scripts

--- modules/weechat/install.sh
+++
@@ -1,6 +0,0 @@
-set -e
-apt install weechat-python python3-websocket
-
-mkdir -p ~/.weechat/python/autoload
-curl https://raw.githubusercontent.com/wee-slack/wee-slack/master/wee_slack.py -o $HOME/.weechat/python/autoload/wee_slack.py
-curl https://raw.githubusercontent.com/wee-slack/wee-slack/master/weemoji.json -o $HOME/.weechat/weemoji.json
