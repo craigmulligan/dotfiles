5254ded6474309eabb6a920a06587c7333107e0c
Craig Mulligan <git@craigmulligan.com>
2024-09-11 09:19:23 -0400

    add git_rebase to treesitter autoinstall

--- src/.config/nvim/init.lua
+++ src/.config/nvim/init.lua
@@ -955,7 +955,7 @@ require("lazy").setup({
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     opts = {
-      ensure_installed = { "bash", "c", "html", "lua", "luadoc", "markdown", "vim", "vimdoc" },
+      ensure_installed = { "bash", "c", "html", "lua", "luadoc", "markdown", "vim", "vimdoc", "diff", "git_rebase" },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
